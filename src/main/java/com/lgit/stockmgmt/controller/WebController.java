1fc6391ccc376bd219adea7a420c261ba682ec50
박재용 (jaeyong1.park) <jaeyong1.park@lginnotek.com>
2016-09-20 11:49:42 +0900

    jsp show db table..  /itemlist

--- src/main/java/com/lgit/stockmgmt/controller/WebController.java
+++ src/main/java/com/lgit/stockmgmt/controller/WebController.java
@@ -5,13 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.lgit.stockmgmt.domain.Item;
 import com.lgit.stockmgmt.service.ItemService;
-
 
 @Controller
 public class WebController {
@@ -35,7 +36,6 @@ public class WebController {
 		return "itemlist_test";
 	}
 
-	
 	/*
 	 * Controller - Service 연결 예시
 	 */
@@ -45,29 +45,14 @@ public class WebController {
 	@RequestMapping(value = "/itemlist", method = RequestMethod.GET)
 	public String getItemList(Model model) {
 		List<Item> items = itemService.getItems(); // 위에서 Autowired로 연결=객체생성
-		model.addAttribute("name", "itemlist John Lee"); // simple value. jsp코드 : ${name}
-		System.out.println("itemlist process");
-		
-		if (!items.isEmpty()) {
-			System.out.println(items.get(0).getDevId());
-			System.out.println(items.get(0).getProjecName());
-		}
-		model.addAttribute("items", items);  //오브젝트로 넘어가는데  jsp에서 출력??
-		return "itemlist"; /* itemlist.jsp */	
+		System.out.println("stocklist process");
+
+		model.addAttribute("name", "John Lee"); // simple value. jsp코드
+		model.addAttribute("items", items);
+		return "stocklist"; /* stocklist.jsp */
 	}
 
-	@RequestMapping("/listtest")
-	public ModelAndView listProcess() {
 
-		ModelAndView model = new ModelAndView("itemlist");
-		List<Item> items = itemService.getItems(); // 위에서 Autowired로 연결=객체생성
-		System.out.println("/listtest");
-		
-		
-		model.addObject("name", "Test Park");
-		model.addObject("items", items);
-
-		return model;
-	}
-
+	
+	
 }
